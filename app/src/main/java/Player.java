962f69c1706b2ceac8decd65b4857dc5f4fa8cea
Claire Gutierrez <crystalclairenight@gmail.com>
2016-01-14 08:49:21 +0000

    Ironing out warnings and code revision notes from IDE
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@8 da46e613-9826-43f3-9a83-6061620c19e0

--- app/src/main/java/Player.java
+++ app/src/main/java/Player.java
@@ -6,42 +6,20 @@ import com.rezdron.chris.agame.Token;
  * Carries player token specific data including special physics functions
  */
 public class Player extends Token {
-    TokenPhysics phys;
-
-
     Player()
     {
-        super(32,0);
-        phys = new PhysVert(32,64,(float)0.0,(float)0.0);
+        super(0, new PhysVert(32,48,(float)0.0,(float)0.0));
     }
 
-    public void setAccel(Integer x, Integer y)
+    public void setAccel(Integer y)
     {
-        dvx = dvx + x;
-        dvy = dvy + y;
+        //phys.applyDvx(x);
+        phys.applyDvy(y);
     }
 
-    @override
     public void tick()
     {
-        // Player always subject to gravity
-        // 8px = 1m
-        // Assuming gravity (in water) acceleration of about
-        // 5.4m/s^2, a^2t + bt + c, x = c dvx = b, 5.4 = a
         phys.tick();
-        // human interaction will alter dvy
-        // Player token has no other physics functions
-        // Map will handle the x axis  of background and other tokens will advance towards player manually
-
     }
 
-    public Integer getX()
-    {
-        return phys.getX();
-    }
-
-    public Integer getY()
-    {
-        return phys.getY();
-    }
 }
