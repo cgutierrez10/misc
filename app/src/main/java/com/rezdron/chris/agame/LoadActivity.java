73f78e2f05d5a34cfb2eb9d76440365ded0e61ff
Claire Gutierrez <crystalclairenight@gmail.com>
2016-01-21 04:53:28 +0000

    Very basic implementation of ability to activity switch, ie title screen to loading screen.
    Will need to be more fully fleshed out later.
    
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@11 da46e613-9826-43f3-9a83-6061620c19e0

---
+++ app/src/main/java/com/rezdron/chris/agame/LoadActivity.java
@@ -0,0 +1,56 @@
+package com.rezdron.chris.agame;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Chris on 1/20/2016.
+ * Activity for the load screen
+ */
+public class LoadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load);
+
+        /*
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+        */
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle app bar item clicks here. The app bar
+        // automatically handles clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.load,
+                    container, false);
+            return rootView;
+        }
+    }
+}
