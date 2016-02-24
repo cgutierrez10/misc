634284b6be526c67e64f8c57ba4aeebe75c119f0
Claire Gutierrez <crystalclairenight@gmail.com>
2016-02-24 05:36:37 +0000

    Removing junk file, will rename over it in next version
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@25 da46e613-9826-43f3-9a83-6061620c19e0

--- app/src/main/java/com/rezdron/chris/agame/LoadView.java
+++
@@ -1,9 +0,0 @@
-package com.rezdron.chris.agame;
-
-/**
- * Created by Chris on 1/12/2016.
- * The loading state should initialize and reset the tokens score, player state map
- * It should also build a pauseView if this has not been done to allow for faster state switch to pause
- */
-public class LoadView {
-}
