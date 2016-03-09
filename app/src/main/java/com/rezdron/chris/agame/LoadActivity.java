0cedfc01eccccbb0d5e2e148d1f92f55478f9131
Claire Gutierrez <crystalclairenight@gmail.com>
2016-03-09 12:38:59 +0000

    Started rewrite for opengl based rendering. No compiler errors but untested.
    Not complete enough to view or test any changes yet.
    
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@30 da46e613-9826-43f3-9a83-6061620c19e0

--- app/src/main/java/com/rezdron/chris/agame/LoadActivity.java
+++ app/src/main/java/com/rezdron/chris/agame/LoadActivity.java
@@ -2,6 +2,7 @@ package com.rezdron.chris.agame;
 
 import android.app.Fragment;
 import android.graphics.Canvas;
+import android.opengl.GLSurfaceView;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,22 +11,41 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Chris on 1/20/2016.
  * Activity for the activity_load screen
  */
 public class LoadActivity extends AppCompatActivity {
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /* Some changes for GL support */
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        glSurfaceView = new LoadBusyView(this);
         setContentView(R.layout.activity_load);
         // Ensure player is created
         Player.getInstance();
         //May need to replace, and reactivate the player token on game start/ends
         //Player.revive(); // Set player active to true
         //Player.place();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
     }
 
     @Override
