c2b835003974261b4a73988cf9ceb3edb4d687e6
Claire Gutierrez <crystalclairenight@gmail.com>
2016-03-29 04:51:10 +0000

    More modifying, started writing back to activity and thread loop level. Stripped out previous canvas functionality. Able to load without errors and switch to blank screen not rendering anything.
    Next step to incorporate necessary changes to make gl functions available to loops and start drawing again.
    
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@33 da46e613-9826-43f3-9a83-6061620c19e0

--- app/src/main/java/com/rezdron/chris/agame/GLRenderer.java
+++ app/src/main/java/com/rezdron/chris/agame/GLRenderer.java
@@ -87,12 +87,13 @@ public class GLRenderer implements Renderer {
         GLES20.glUniformMatrix4fv(mtrxhandle, 1, false, m, 0);
         int mSamplerLoc = GLES20.glGetUniformLocation(SpriteShader.sp_Sprite, "s_texture" );
 
-        GLES20.glUniform1i(mSamplerLoc,0);
+        GLES20.glUniform1i(mSamplerLoc, 0);
 
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, indices.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
 
         GLES20.glDisableVertexAttribArray(mPositionHandle);
         GLES20.glDisableVertexAttribArray(mTexCoordLoc);
+
     }
 
     @Override public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -104,14 +105,12 @@ public class GLRenderer implements Renderer {
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
         gl.glOrthof(0.0f, width, 0.0f, height, 0.0f, 1.0f);
-
         gl.glShadeModel(GL10.GL_FLAT);
         gl.glEnable(GL10.GL_BLEND);
         gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
         gl.glColor4x(0x10000, 0x10000, 0x10000, 0x10000);
         gl.glEnable(GL10.GL_TEXTURE_2D);
         /* End fragment added */
-
         // Ensure viewport is fullscreen
         GLES20.glViewport(0, 0, (int) mScreenWidth, (int) mScreenHeight);
 
@@ -163,7 +162,7 @@ public class GLRenderer implements Renderer {
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
 
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,GLES20.GL_TEXTURE_WRAP_S,GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
 
         GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, GfxResourceHandler.getInstance().getRsx("spider"), 0);
@@ -181,6 +180,130 @@ public class GLRenderer implements Renderer {
         vertexBuffer.put(vertices);
         vertexBuffer.position(0);
 
+        ByteBuffer dlb = ByteBuffer.allocateDirect(indices.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(indices);
+        drawListBuffer.position(0);
+    }
+
+    void RenderSprite(int spritesheet, int spritex, int spritey, int texturew, int textureh, int x, int y, int w, int h )
+    {
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,spritesheet);
+        // Texturing, select texels from atlas
+        uvs = new float[8];
+        uvs[0] = 0;
+        uvs[1] = 0;
+        uvs[2] = 0;
+        uvs[3] = 1;
+        uvs[4] = 1;
+        uvs[5] = 1;
+        uvs[6] = 1;
+        uvs[7] = 0;
+
+        // Texture Buffer
+        /* 4 bytes per float? Always true on android? */
+        ByteBuffer bb = ByteBuffer.allocateDirect(uvs.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        uvBuffer = bb.asFloatBuffer();
+        uvBuffer.put(uvs);
+        uvBuffer.position(0);
+
+        // Texture gen
+        int[] texturenames = new int[1];
+        GLES20.glGenTextures(1, texturenames, 0);
+
+        // Retrieve image from rsx, probably need to modify this to get lowerleft + h/w
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texturenames[0]);
+
+        // Set Filter
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,GLES20.GL_CLAMP_TO_EDGE);
+
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, GfxResourceHandler.getInstance().getRsx("sheep"), 0);
+
+        /*
+        glColor4ub(255, 255, 255, 255);
+        glBindTexture(GL_TEXTURE_2D, spritesheet);
+        glBegin(GL_QUADS);
+        glTexCoord2d(spritex / (double) texturew, spritey / (double) textureh);
+        glVertex2f(x, y);
+        glTexCoord2d((spritex + w) / (double) texturew, spritey / (double) textureh);
+        glVertex2f(x + w, y);
+        glTexCoord2d((spritex + w) / (double) texturew, (spritey + h) / (double) textureh);
+        glVertex2f(x + w, y + h);
+        glTexCoord2d(spritex / (double) texturew, (spritey + h) / (double) textureh);
+        glVertex2f(x, y + h);
+        gl.glEnd();
+        */
+    }
+
+    void RenderQuads()
+    {
+        // 32x32 triangles quad
+        vertices = new float[]
+                {  10.0f, 42f, 0.0f,
+                        10.0f, 10.0f, 0.0f,
+                        42f, 10.0f, 0.0f,
+                        42f, 42f, 0.0f,
+                };
+
+        indices = new short[] {0,1,2,0,2,3};
+
+
+        /* This should be passed in as parameter */
+        /* Also including extra 4 points indicating atlas frame? */
+        /* Possibly 6 ints, <x,y> drawat, <x,y> drawfrom, height/width ? */
+        // Below is fuller example above is minimum to test functionality
+        /*
+        vertices = new float[12];
+        for(int i=0;i<1;i++)
+        {
+            vertices[(i*12) + 0] = 1;
+            vertices[(i*12) + 1] = 33;
+            vertices[(i*12) + 2] = 0;
+
+            vertices[(i*12) + 3] = 1;
+            vertices[(i*12) + 4] = 1;
+            vertices[(i*12) + 5] = 0;
+
+            vertices[(i*12) + 6] = 33;
+            vertices[(i*12) + 7] = 1;
+            vertices[(i*12) + 8] = 0;
+
+            vertices[(i*12) + 9] = 33;
+            vertices[(i*12) + 10] = 33;
+            vertices[(i*12) + 11] = 0;
+        }
+
+
+        indices = new short[6];
+        int last = 0;
+        for(int i=0;i<6;i++)
+        {
+            indices[(i*6) + 0] = (short) (last + 0);
+            indices[(i*6) + 1] = (short) (last + 1);
+            indices[(i*6) + 2] = (short) (last + 2);
+            indices[(i*6) + 3] = (short) (last + 0);
+            indices[(i*6) + 4] = (short) (last + 2);
+            indices[(i*6) + 5] = (short) (last + 3);
+            last = last + 4;
+        }
+        */
+
+        // vertices is triangle pair forming quad
+        // Vertex buffer
+        ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
+
+        // Init byte buffer draw list
         ByteBuffer dlb = ByteBuffer.allocateDirect(indices.length * 2);
         dlb.order(ByteOrder.nativeOrder());
         drawListBuffer = dlb.asShortBuffer();
