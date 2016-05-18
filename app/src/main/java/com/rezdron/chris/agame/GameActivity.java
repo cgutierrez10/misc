617e6f7bac47f614d87087e316b2a64d5bd873ed
Claire Gutierrez <crystalclairenight@gmail.com>
2016-05-18 02:16:03 +0000

    Building out transitions between activities. Groundwork for pause and renaming loadbusyview to gameview, will modify to take setting for loading or game modes.
    Next target adding a pause button to the gamemode and make transition from loading to gamemode and test out pause function/popup.
    
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@49 da46e613-9826-43f3-9a83-6061620c19e0

--- app/src/main/java/com/rezdron/chris/agame/GameActivity.java
+++ app/src/main/java/com/rezdron/chris/agame/GameActivity.java
@@ -5,13 +5,14 @@ import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.PopupWindow;
 
 /**
  * Created by Chris on 1/20/2016.
@@ -29,7 +30,7 @@ public class GameActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Player.getInstance();
 
-        glSurfaceView = new LoadBusyView(this);
+        glSurfaceView = new GameView(this);
         setContentView(glSurfaceView);
 
         //May need to replace, and reactivate the player token on game start/ends
@@ -41,6 +42,19 @@ public class GameActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         glSurfaceView.onPause();
+
+        try {
+            //We need to get the instance of the LayoutInflater, use the context of this activity
+            LayoutInflater inflater = (LayoutInflater) this.getSystemService(this.getApplicationContext().LAYOUT_INFLATER_SERVICE);
+            //Inflate the view from a predefined XML layout
+            View layout = inflater.inflate(R.layout.activity_pause, (ViewGroup) findViewById(R.id.pause_pop));
+            // create a 300px width and 470px height PopupWindow
+            PopupWindow pw = new PopupWindow(layout, 300, 470, true);
+            // display the popup in the center
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
