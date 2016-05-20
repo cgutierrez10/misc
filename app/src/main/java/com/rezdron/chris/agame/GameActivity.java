6e2559c4001e603151a6bd4aaa36c46180377a91
Claire Gutierrez <crystalclairenight@gmail.com>
2016-05-20 05:36:55 +0000

    Bit of debugging and filling out activity transitions.
    Need to overlay the pause button to test transition to pause.
    
    opengl view/threads/content need to wipe between load to game run.
    
    git-svn-id: https://laptop-1j1vpkuu/svn/agame@50 da46e613-9826-43f3-9a83-6061620c19e0

--- app/src/main/java/com/rezdron/chris/agame/GameActivity.java
+++ app/src/main/java/com/rezdron/chris/agame/GameActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -23,10 +24,11 @@ public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
+        Log.d("ActivityMode", "About to create renderer for GameActivity");
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Player.getInstance();
 
