f22981b9565e2a911e5ed8d44363ceaa3e67f528
Claire Gutierrez <crystalclairenight@gmail.com>
2019-12-13 00:11:02 -0700

    Significant progress. Coded to convert raw commands to vertex list and mostly implimented vertex list to an optimized walk. Simple version of walk to grbl jog codes is present but not complete. It would turn each vertex into an absolute jog rather than tracking current location and adding the walk to that.

--- src/triplet.java
+++ src/triplet.java
@@ -1,11 +1,40 @@
 
 public class triplet {
+	int x;
+	int y;
+	int z;
 	triplet(int a, int b, int c) {
 		x = a;
 		y = b;
 		z = c;
 	}
-	int x;
-	int y;
-	int z;
+	
+	// This int created can be used for loose computations but should not be stored in the vertex list
+	triplet(double a, double b, double c) {
+		System.out.println("Warning, created int triplet from doubles");
+		this.x = (int) a;
+		this.y = (int) b;
+		this.z = (int) c;
+	}
+	
+	public triplet add(triplet plus) {
+		return new triplet(this.x + plus.x, this.y + plus.y, this.z + plus.z);
+	}
+	
+	public triplet sub(triplet plus) {
+		return this.add(plus.mult(-1));
+	}
+	
+	public triplet mult(triplet plus) {
+		return new triplet(this.x + plus.x, this.y + plus.y, this.z + plus.z);
+	}
+	
+	public triplet mult(float scale) {
+		return new triplet(this.x * scale, this.y * scale, this.z * scale);
+	}
+	
+	public double norm() {
+		// return normalized scalar
+		return Math.sqrt((this.x * this.x) + (this.y * this.y) + (this.z * this.z));
+	}
 }
